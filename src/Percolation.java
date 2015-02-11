2638d396ea629dab7e77750a0c87615bea438a5e
Igor Arouca <igor.arouca@liferay.com>
2015-02-11 15:20:33 -0600

    remove Grid class and make Site an inner class of Percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -25,9 +25,15 @@
  */
 public class Percolation {
 
-	private Grid grid;
+	private static final int VIRTUAL_TOP_SITE_INDEX = 0;
 
-	// create N-by-N grid, with all sites blocked
+	private int n;		// number of sites
+	private Site[][] sites;	// 2-dimensional grid of sites
+
+	// Data structure to support union-find operations on the grid.
+	// If there was an interface UnionFind, this object could be dynamically set
+	private WeightedQuickUnionUF unionFind;
+
 	/**
      * Initializes an empty Percolation data structure with (N * N) sites.
      * All sites are initially BLOCKED.
@@ -35,7 +41,56 @@ public class Percolation {
      * @param N the number of objects
      */
 	public Percolation(int N) {
-		this.grid = new Grid(N);
+		if (N <= 0) {
+			throw new IllegalArgumentException(
+				"Invalid grid size: " + N + 
+				". Grid size (n) has to be greater than zero.");
+		}
+
+		this.n = N;
+
+		initializeSites();
+		initializeUnionFindStructure();
+	}
+
+	private void initializeSites() {
+		this.sites = new Site[n][n];
+
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				sites[i][j] = new Site(i, j);
+	}
+
+	private void initializeUnionFindStructure() {
+		// creates a union-find structure with 2 extra nodes for virtual sites
+		this.unionFind = new WeightedQuickUnionUF((n * n) + 2);
+
+		createConnectionsToVirtualTopSite();
+		createConnectionsToVirtualBottomSite();
+	}
+
+	private void createConnectionsToVirtualTopSite() {
+		Site[] topRowSites = sites[0];
+
+		for (Site site : topRowSites)
+			unionFind.union(
+				getVirtualTopSiteUFIndex() , site.getUnionFindIndex());
+	}
+
+	private int getVirtualTopSiteUFIndex() {
+		return VIRTUAL_TOP_SITE_INDEX;
+	}
+
+	private void createConnectionsToVirtualBottomSite() {
+		Site[] bottomRowSites = sites[n - 1];
+
+		for (Site site : bottomRowSites)
+			unionFind.union(
+				getVirtualBottomUFSiteIndex(), site.getUnionFindIndex());
+	}
+
+	private int getVirtualBottomUFSiteIndex() {
+		return (n * n) + 1;
 	}
 
 	/** Open site (i, j) if it is not open already
@@ -44,7 +99,7 @@ public class Percolation {
 	 * @param j
 	 */
 	public void open(int i, int j) {
-		grid.getSiteAt(--i, --j).open();
+		sites[--i][--j].open();
 	}
 
 	/** Is site (i, j) open?
@@ -54,7 +109,7 @@ public class Percolation {
 	 * @return true if the site is open
 	 */
 	public boolean isOpen(int i, int j) {
-		return grid.getSiteAt(--i, --j).isOpen();
+		return sites[--i][--j].isOpen();
 	}
 
 	/** Is site (i, j) full?
@@ -64,7 +119,7 @@ public class Percolation {
 	 * @return true if the site is full
 	 */
 	public boolean isFull(int i, int j) {
-		return grid.getSiteAt(--i, --j).isFull();
+		return sites[--i][--j].isFull();
 	}
 
 	/** Does the system percolate?
@@ -73,266 +128,157 @@ public class Percolation {
 	 * @return true if the system percolates
 	 */
 	public boolean percolates() {
-		return grid.percolates();
-	}
-
-}
-
-/**
- * The <tt>Grid</tt> class represents a 2-dimensional grid of sites. 
- * Each site is represented by an instance of <code>Site</code>.
- * Grid positions are zero-based, ranging from (0,0) to (n-1,n-1).
- * This implementation supports union-find operations on the grid's sites.
- * 
- * @author Igor Arouca
- * @version 1.0
- * @since 2015-02-10
- */
-class Grid {
-
-	private static final int VIRTUAL_TOP_SITE_INDEX = 0;
-
-	private int size;		// number of sites
-	private Site[][] sites;	// 2-dimensional grid of sites
-
-	// Data structure to support union-find operations on the grid.
-	// If there was an interface UnionFind, this object could be dynamically set
-	private WeightedQuickUnionUF unionFind;
-
-	/**
-	 * Initializes a n-by-n grid with all sites initially blocked 
-	 * 
-	 * @param n the size of this 2-dimensional grid (n-by-n)
-	 */
-	Grid(int n) {
-		if (n <= 0) {
-			throw new IllegalArgumentException(
-				"Invalid grid size: " + n + 
-				". Grid size (n) has to be greater than zero."
-			);
-		}
-
-		this.size = n;
-
-		initializeSites();
-		initializeUnionFindStructure();
-	}
-
-	private void initializeSites() {
-		this.sites = new Site[size][size];
-
-		for (int i = 0; i < size; ++i) {
-			for (int j = 0; j < size; ++j) {
-				sites[i][j] = new Site(this, i, j);
-			}
-		}
-	}
-
-	private void initializeUnionFindStructure() {
-
-		// creates a union-find structure with 2 extra nodes for virtual sites
-		this.unionFind = new WeightedQuickUnionUF((size * size) + 2);
-
-		createConnectionsToVirtualTopSite();
-		createConnectionsToVirtualBottomSite();
-	}
-
-	private void createConnectionsToVirtualTopSite() {
-		Site[] topRowSites = sites[0];
-
-		for (Site site : topRowSites) {
-			unionFind.union(
-				getVirtualTopSiteUFIndex() , site.getUnionFindIndex()
-			);
-		}
-	}
-
-	int getVirtualTopSiteUFIndex() {
-		return VIRTUAL_TOP_SITE_INDEX;
-	}
-
-	private void createConnectionsToVirtualBottomSite() {
-		Site[] bottomRowSites = sites[size - 1];
-
-		for (Site site : bottomRowSites) {
-			unionFind.union(
-				getVirtualBottomUFSiteIndex(), site.getUnionFindIndex()
-			);
-		}
-	}
-
-	private int getVirtualBottomUFSiteIndex() {
-		return (size * size) + 1;
-	}
-
-	/**
-	 * Get site at provided location (row,column)
-	 * 
-	 * @param row row index between 0 and (size - 1)
-	 * @param column column index between 0 and (size - 1)
-	 * @return site at location (row,column)
-	 */	
-	public Site getSiteAt(int row, int column) {
-		return sites[row][column];
-	}
-
-	/**
-	 * Get size of the grid.
-	 * If size = n, grid is n-by-n
-	 * 
-	 * @return size of the grid
-	 */
-	public int getSize() {
-		return sites.length;
-	}
-
-	WeightedQuickUnionUF getUnionFind() {
-		return unionFind;
-	}
-
-	/**
-	 * Determines if the grid percolates by checking if the virtual top site
-	 * is connected to the virtual bottom site.
-	 * 
-	 * @return true if the grid percolates or false otherwise
-	 */
-	public boolean percolates() {
 		return unionFind.connected(
-			getVirtualBottomUFSiteIndex(), getVirtualTopSiteUFIndex()
-		);
-	}
-
-}
-
-class Site {
-
-	private static enum Status { BLOCKED, OPEN };
-
-	private int row;
-	private int column;
-
-	private Grid grid;
-
-	private Status status;
-
-	Site(Grid grid, int row, int column) {
-		if (row < 0 || row > grid.getSize()) {
-			throw new ArrayIndexOutOfBoundsException(
-				"row index is out of bounds: " + row
-			);
-		}
-
-		if (column < 0 || column > grid.getSize()) {
-			throw new ArrayIndexOutOfBoundsException(
-				"column index is out of bounds: " + column
-			);
-		}
-
-		this.row = row;
-		this.column = column;
-
-		this.grid = grid;
-
-		this.status = Status.BLOCKED;
-	}
-
-	public boolean isFull() {
-		return grid.getUnionFind().connected(
-			grid.getVirtualTopSiteUFIndex(), this.getUnionFindIndex()
-		);
-	}
-
-	public boolean isOpen() {
-		return status == Status.OPEN;
-	}
-
-	public void open() {
-		if (isOpen()) return;
-
-		this.status = Status.OPEN;
-		connectToOpenAdjacentSites();
-	}
-
-	public void connectToOpenAdjacentSites() {
-		connectTo(getTopAdjacentSite());
-		connectTo(getRightAdjacentSite());
-		connectTo(getBottomAdjacentSite());
-		connectTo(getLeftAdjacentSite());
-	}
-
-	public void connectTo(Site adjacent) {
-		if (adjacent == null || !adjacent.isOpen()) return;
-
-		grid.getUnionFind().union(
-			getUnionFindIndex(), adjacent.getUnionFindIndex());
-	}
-
-	public int getUnionFindIndex() {		
-		return ((grid.getSize() * row) + column) + 1;
-	}
-
-	public Site getLeftAdjacentSite() {
-		Site adjacent = null;
-
-		if (isNotAtFirstColumn()) {
-			adjacent = grid.getSiteAt(row, column - 1);
-		}
-
-		return adjacent;
-	}
-
-	private boolean isNotAtFirstColumn() {
-		return column > 0;
-	}
-
-	public Site getRightAdjacentSite() {
-		Site adjacent = null;
-
-		if (isNotAtLastColumn()) {
-			grid.getSiteAt(row, column + 1);
-		}
-
-		return adjacent;
-	}
-
-	private boolean isNotAtLastColumn() {
-		return column < grid.getSize() - 1;
-	}
-
-	public Site getTopAdjacentSite() {
-		Site adjacent = null;
-
-		if (isNotAtFirstRow()) {
-			adjacent = grid.getSiteAt(row - 1, column);
-		}
-
-		return adjacent;
-	}
-
-	private boolean isNotAtFirstRow() {
-		return row > 0;
-	}
-
-	public Site getBottomAdjacentSite() {
-		Site adjacent = null;
-
-		if (isNotAtLastRow()) {
-			adjacent = grid.getSiteAt(row + 1, column);
-		}
-
-		return adjacent;
-	}
-
-	private boolean isNotAtLastRow() {
-		return row < grid.getSize() - 1;
+			getVirtualBottomUFSiteIndex(), getVirtualTopSiteUFIndex());
 	}
 
 	/**
-	 * return string with Site location and status: (row,column) --> STATUS
+	 * The <tt>Site<tt> class represents a site in the percolation grid.
+	 * Each site instance has a defined position (row, column) in the grid 
+	 * and a status: either OPEN or BLOCKED. Sites always start in the BLOCKED
+	 * status, and can be opened using the <code>open</code> method.
+	 * 
 	 */
-	@Override
-	public String toString() {
-		return "(" + this.row + "," + this.column + ")" + " --> " + this.status;
+	class Site {
+
+		/* Inner classes don't accept enum types, so the constants below
+		 * are used to represent the possible statuses of a site
+		 */
+		
+		private static final int BLOCKED = 0;
+		private static final int OPEN = 1;
+
+		private int row;						// row number from 0 to n-1
+		private int column;						// column number from 0 to n-1
+
+		private int status;						// status of the site
+
+		Site(int row, int column) {
+			if (row < 0 || row > n) {
+				throw new ArrayIndexOutOfBoundsException(
+					"row index is out of bounds: " + row
+				);
+			}
+
+			if (column < 0 || column > n) {
+				throw new ArrayIndexOutOfBoundsException(
+					"column index is out of bounds: " + column
+				);
+			}
+
+			this.row = row;
+			this.column = column;
+
+			this.status = BLOCKED;
+		}
+
+		/**
+		 * Check if site is connected to one of the sites in the top row.
+		 * 
+		 * @return true if site is full and false otherwise
+		 */
+		boolean isFull() {
+			return unionFind.connected(
+				getVirtualTopSiteUFIndex(), this.getUnionFindIndex()
+			);
+		}
+
+		boolean isOpen() {
+			return status == OPEN;
+		}
+
+		/**
+		 * Change site status to OPEN and connect site 
+		 * to all open adjacent sites
+		 */
+		void open() {
+			if (isOpen()) return;
+
+			this.status = OPEN;
+			connectToOpenAdjacentSites();
+		}
+
+		private void connectToOpenAdjacentSites() {
+			connectTo(getTopAdjacentSite());
+			connectTo(getRightAdjacentSite());
+			connectTo(getBottomAdjacentSite());
+			connectTo(getLeftAdjacentSite());
+		}
+
+		private void connectTo(Site adjacent) {
+			if (adjacent == null || !adjacent.isOpen()) return;
+	
+			unionFind.union(getUnionFindIndex(), adjacent.getUnionFindIndex());
+		}
+	
+		private int getUnionFindIndex() {		
+			return ((n * row) + column) + 1;
+		}
+
+		Site getLeftAdjacentSite() {
+			Site adjacent = null;
+
+			if (isNotAtFirstColumn()) {
+				adjacent = sites[row][column - 1];
+			}
+
+			return adjacent;
+		}
+
+		private boolean isNotAtFirstColumn() {
+			return column > 0;
+		}
+
+		Site getRightAdjacentSite() {
+			Site adjacent = null;
+
+			if (isNotAtLastColumn()) {
+				adjacent = sites[row][column + 1];
+			}
+
+			return adjacent;
+		}
+
+		private boolean isNotAtLastColumn() {
+			return column < n - 1;
+		}
+
+		Site getTopAdjacentSite() {
+			Site adjacent = null;
+
+			if (isNotAtFirstRow()) {
+				adjacent = sites[row - 1][column];
+			}
+
+			return adjacent;
+		}
+
+		private boolean isNotAtFirstRow() {
+			return row > 0;
+		}
+
+		Site getBottomAdjacentSite() {
+			Site adjacent = null;
+	
+			if (isNotAtLastRow()) {
+				adjacent = sites[row + 1][column];
+			}
+	
+			return adjacent;
+		}
+	
+		private boolean isNotAtLastRow() {
+			return row < n - 1;
+		}
+
+		/**
+		 * return string with Site location and status: (row,column) --> STATUS
+		 */
+		@Override
+		public String toString() {
+			return "(" + this.row + "," + this.column + ")" + 
+				" --> " + this.status;
+		}
 	}
 
 }
